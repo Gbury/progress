0be9e047845b700a59262b77a1725d49591a010e
Guillaume Bury <guillaume.bury@gmail.com>
2023-01-03 16:06:09 +0100

    Revert "Update for mtime 2.0"
    This reverts commit 728e0d8f7505d7dd1bf7904982c59d47de9cca66.

--- src/progress/engine/flow_meter.ml
+++ src/progress/engine/flow_meter.ml
@@ -75,8 +75,7 @@ let per_second : type a. a t -> a =
     let interval =
       let start_time = t.timestamps.(oldest_index) in
       let end_time = t.timestamps.(t.most_recently_added) in
-      let span_ns = Mtime.Span.to_float_ns (Mtime.span start_time end_time) in
-      span_ns /. (10. ** 9.)
+      Mtime.Span.to_s (Mtime.span start_time end_time)
     in
     if Float.compare interval Float.epsilon < 0 then Integer.zero
     else
